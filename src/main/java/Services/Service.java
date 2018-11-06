e00096e3b9039cfdbfda2e64abeecb3905ef674d
willi <willi@DESKTOP-USN773L>
2018-11-06 16:21:19 -0500

    http get

--- src/main/java/Services/Service.java
+++ src/main/java/Services/Service.java
@@ -11,6 +11,7 @@ import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.util.EntityUtils;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import java.io.BufferedReader;
@@ -27,24 +28,27 @@ public class Service {
     public JsonObject getTipos()
             throws ClientProtocolException, IOException {
         System.out.println("entroooo");
-        String url1 = "https://servicemecatobot.herokuapp.com/myApp/rest/products";
+        String url = "https://servicemecatobot.herokuapp.com/myApp/rest/products";
 
-        URL url = new URL(url1);
-        HttpURLConnection con = (HttpURLConnection) url.openConnection();
-        con.setRequestMethod("GET");
-        con.setRequestProperty("Content-Type", "application/json");
-        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-        String inputLine;
-        StringBuffer content = new StringBuffer();
-        while ((inputLine = in.readLine()) != null) {
-            content.append(inputLine);
+         //step 2: Create a HTTP client
+        HttpClient httpclient = HttpClientBuilder.create().build();
+
+        //step 3: Create a HTTPGet object and execute the url
+        HttpGet httpGet = new HttpGet(url);
+        HttpResponse response = httpclient.execute(httpGet);
+
+        //step 4: Process the result
+        JsonObject json = null;
+        int statusCode = response.getStatusLine().getStatusCode();
+        if (statusCode == 200) {
+            System.out.println("funcooooooooo");
+            String response_string = EntityUtils.toString(response.getEntity());
+            json = (new JsonParser()).parse(response_string)
+                    .getAsJsonObject();
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+            String prettyJson = gson.toJson(json);
+            System.out.println(prettyJson);
         }
-        System.out.println("mensaje: "+content);
-        in.close();
-        con.disconnect();
-        JsonParser parser = new JsonParser();
-
-        JsonObject json= (JsonObject) parser.parse(content.toString());
         return json;
     }
 
@@ -52,24 +56,26 @@ public class Service {
             throws ClientProtocolException, IOException {
 
         //step 1: Prepare the url
-        String url1 = "https://servicemecatobot.herokuapp.com/myApp/rest/products/" + tipo;
+        String url = "https://servicemecatobot.herokuapp.com/myApp/rest/products/" + tipo;
 
-         URL url = new URL(url1);
-        HttpURLConnection con = (HttpURLConnection) url.openConnection();
-        con.setRequestMethod("GET");
-        con.setRequestProperty("Content-Type", "application/json");
-        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-        String inputLine;
-        StringBuffer content = new StringBuffer();
-        while ((inputLine = in.readLine()) != null) {
-            content.append(inputLine);
+        //step 2: Create a HTTP client
+        HttpClient httpclient = HttpClientBuilder.create().build();
+
+        //step 3: Create a HTTPGet object and execute the url
+        HttpGet httpGet = new HttpGet(url);
+        HttpResponse response = httpclient.execute(httpGet);
+
+        //step 4: Process the result
+        JsonObject json = null;
+        int statusCode = response.getStatusLine().getStatusCode();
+        if (statusCode == 200) {
+            String response_string = EntityUtils.toString(response.getEntity());
+            json = (new JsonParser()).parse(response_string)
+                    .getAsJsonObject();
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+            String prettyJson = gson.toJson(json);
+            System.out.println(prettyJson);
         }
-        System.out.println(content);
-        in.close();
-        con.disconnect();
-        JsonParser parser = new JsonParser();
-
-        JsonObject json= (JsonObject) parser.parse(content.toString());
         return json;
     }
 
@@ -77,24 +83,26 @@ public class Service {
             throws ClientProtocolException, IOException {
 
         //step 1: Prepare the url
-        String url1 = "https://servicemecatobot.herokuapp.com/myApp/rest/tienda/" + tipo + "/" + ing;
+        String url = "https://servicemecatobot.herokuapp.com/myApp/rest/tienda/" + tipo + "/" + ing;
 
-         URL url = new URL(url1);
-        HttpURLConnection con = (HttpURLConnection) url.openConnection();
-        con.setRequestMethod("GET");
-        con.setRequestProperty("Content-Type", "application/json");
-        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-        String inputLine;
-        StringBuffer content = new StringBuffer();
-        while ((inputLine = in.readLine()) != null) {
-            content.append(inputLine);
+        //step 2: Create a HTTP client
+        HttpClient httpclient = HttpClientBuilder.create().build();
+
+        //step 3: Create a HTTPGet object and execute the url
+        HttpGet httpGet = new HttpGet(url);
+        HttpResponse response = httpclient.execute(httpGet);
+
+        //step 4: Process the result
+        JsonObject json = null;
+        int statusCode = response.getStatusLine().getStatusCode();
+        if (statusCode == 200) {
+            String response_string = EntityUtils.toString(response.getEntity());
+            json = (new JsonParser()).parse(response_string)
+                    .getAsJsonObject();
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+            String prettyJson = gson.toJson(json);
+            System.out.println(prettyJson);
         }
-        System.out.println(content);
-        in.close();
-        con.disconnect();
-        JsonParser parser = new JsonParser();
-
-        JsonObject json= (JsonObject) parser.parse(content.toString());
         return json;
     }
 
